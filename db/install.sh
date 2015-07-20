b6345f207f36d2e1e58885d1d51c98068e5191a4
Varun Senguttuvan <varun.seng@gmail.com>
2015-07-20 15:38:44 -0400

    Making changes to install script

--- db/install.sh
+++ db/install.sh
@@ -1,13 +1,16 @@
-#!/bin/bash
+# !/bin/bash
 # Create DB script
 
 POSTGRES_ADMIN=postgres
 DBUSER=postgres
-DBNAME=postgres_magpie
+DBNAME=postgres_db4
 TABLENAME=magpie_cache
 
 sudo -u $POSTGRES_ADMIN createdb -O $DBUSER $DBNAME
 hostName=$(sudo netstat -plunt |grep postgres)
 echo $hostName | cut -d " " -f 4
 
-psql -d $DBNAME -c $"CREATE TABLE IF NOT EXISTS $TABLENAME ( url TEXT PRIMARY KEY NOT NULL, metadata TEXT, created_date datetime default NULL );"
+psql -d $DBNAME -c $"CREATE TABLE IF NOT EXISTS $TABLENAME ( \
+                        url TEXT PRIMARY KEY NOT NULL, \
+                        metadata TEXT, \
+                        created_date timestamp);"
