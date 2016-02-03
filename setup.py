0c475ba6e96fdd33179d1bb365bc74f395940fde
Varun Senguttuvan <varun.seng@gmail.com>
2016-02-03 13:16:51 -0500

    Fixing formatting

--- setup.py
+++ setup.py
@@ -1,6 +1,4 @@
-from setuptools import setup, find_packages
-# To use a consistent encoding
-from codecs import open
+from setuptools import setup
 from os import path
 
 here = path.abspath(path.dirname(__file__))
@@ -13,13 +11,13 @@ setup(
     author='SAAVY',
     author_email='saavy2016@gmail.com',
     license='MIT',
-    keywords=['python','metadata','website','URL'],
+    keywords=['python', 'metadata', 'website', 'URL'],
 
     # Run-time dependencies
-    install_requires=['peppbeautifulsoup4','flake8','flask==0.10.1','lxml','mock','nose','redis','requests','sqlalchemy','wikipediaercorn'],
+    install_requires=['peppbeautifulsoup4', 'flake8', 'flask==0.10.1', 'lxml', 'mock', 'nose', 'redis', 'requests', 'sqlalchemy', 'wikipediaercorn'],
 
     # Additional groups of dependencies here (e.g. test dependencies)
     extras_require={
-        'test': ['coverage','codecov'],
+        'test': ['coverage', 'codecov'],
     },
 )
